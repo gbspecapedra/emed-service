12c4dc6d32b71fb468f9bd489e7b7209f8bdbc80
Gisele Pecapedra <gbspecapedra@gmail.com>
2022-05-12 18:01:27 -0300

    ref: update prescription controller

--- app/Controllers/Http/PrescriptionsController.ts
+++ app/Controllers/Http/PrescriptionsController.ts
@@ -1,3 +1,50 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Prescription from 'App/Models/Prescription'
+import CreatePrescriptionValidator from 'App/Validators/CreatePrescriptionValidator'
 
-export default class PrescriptionsController {}
+export default class PrescriptionsController {
+  public async index() {
+    return await Prescription.all()
+  }
+
+  public async show({ request }: HttpContextContract) {
+    const { id } = request.params()
+    return await Prescription.findOrFail(id)
+  }
+
+  public async create({ request, response }: HttpContextContract) {
+    try {
+      const prescription = await request.validate(CreatePrescriptionValidator)
+
+      return await Prescription.create({ ...prescription })
+    } catch (error) {
+      response.badRequest(error)
+    }
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    try {
+      const { id, ...prescription } = request.all()
+
+      const prescriptionExists = await Prescription.find(id)
+      if (!prescriptionExists) return response.notFound({ message: 'Prescription not found' })
+
+      return await prescriptionExists.merge(prescription).save()
+    } catch (error) {
+      response.internalServerError(error)
+    }
+  }
+
+  public async destroy({ request, response }: HttpContextContract) {
+    try {
+      const { id } = request.params()
+
+      const prescriptionExists = await Prescription.find(id)
+      if (!prescriptionExists) return response.notFound({ message: 'Patient not found' })
+
+      return await prescriptionExists.delete()
+    } catch (error) {
+      response.internalServerError(error)
+    }
+  }
+}
